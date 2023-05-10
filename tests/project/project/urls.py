f5d0f88c65844580fe90b7460f34c898a5f8315a
Jonathan Ehwald <github@ehwald.info>
2023-05-10 18:44:20 +0200

    Redirect to the admin page

--- tests/project/project/urls.py
+++ tests/project/project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
 from admob_ssv.views import AdmobSSVView
 
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/admin/")),
     path("admin/", admin.site.urls),
     path("admob-ssv", AdmobSSVView.as_view()),
 ]
