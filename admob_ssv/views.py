509d68fe809d39380c26aa359a4f08c399c7ca6e
Jonathan Ehwald <github@ehwald.info>
2023-05-11 03:45:02 +0200

    Refactor implementation

--- admob_ssv/views.py
+++ admob_ssv/views.py
@@ -1,15 +1,14 @@
+import hashlib
+import base64
+import urllib
 import math
-from django.http import HttpResponse, HttpResponseBadRequest
+import requests
+from typing import Dict, Optional
+from django.http import HttpResponse, HttpResponseBadRequest, HttpRequest
 from django.views import View
 from django.core.cache import cache
 from admob_ssv.conf import settings
 from admob_ssv.signals import valid_admob_ssv
-from ecdsa import VerifyingKey, BadSignatureError
-from ecdsa.util import sigdecode_der
-import hashlib
-import base64
-import urllib
-import json
 
 
 class AdmobSSVView(View):
@@ -17,69 +16,76 @@ class AdmobSSVView(View):
     KEY_ID_PARAM_NAME = "key_id"
     REQUIRED_PARAM_NAMES = (SIGNATURE_PARAM_NAME, KEY_ID_PARAM_NAME)
 
-    def get(self, request, *args, **kwargs):
-        for query_param_name in self.REQUIRED_PARAM_NAMES:
-            if query_param_name not in request.GET:
+    def get(self, request: HttpRequest) -> HttpResponse:
+        for param_name in self.REQUIRED_PARAM_NAMES:
+            if param_name not in request.GET:
                 return HttpResponseBadRequest()
 
-        # The last two query parameters of rewarded video SSV callbacks are always
-        # signature and key_id, in that order. The remaining query parameters specify
-        # the content to be verified.
         key_id = request.GET[self.KEY_ID_PARAM_NAME]
-        signature = request.GET[self.SIGNATURE_PARAM_NAME]
-        content = request.META["QUERY_STRING"]
-        content = content[: content.index("&" + self.SIGNATURE_PARAM_NAME)]
+        public_key = self.get_public_key(key_id)
 
-        admob_public_keys = cache.get_or_set(
-            settings.ADMOB_SSV_KEYS_CACHE_KEY,
-            self.fetch_public_keys(),
-            math.floor(settings.ADMOB_SSV_KEYS_CACHE_TIMEOUT.total_seconds()),
-        )
-
-        public_key = admob_public_keys.get(key_id, None)
         if public_key is None:
             return HttpResponseBadRequest()
 
-        if self.verify_signature(public_key["pem"], content, signature):
+        signature = self.get_signature(request)
+        content = self.get_unverified_content(request)
+
+        if self.verify_signature(public_key, signature, content):
             valid_admob_ssv.send(sender=None, query=request.GET.dict())
             return HttpResponse()
+
         return HttpResponseBadRequest()
 
-    def fetch_public_keys():
-        keys = dict()
-        url = settings.ADMOB_SSV_KEYS_SERVER_URL
-
-        try:
-            with urllib.request.urlopen(url) as response:
-                if response.getcode() != 200:
-                    return keys
-                data = response.read().decode()
-        except urllib.URLError:
-            return keys
-
-        json_data = json.loads(data)
-
-        for key in json_data["keys"]:
-            keys[str(key["keyId"])] = {
-                "pem": key["pem"],
-                "base64": key["base64"],
-            }
-        return keys
-
-    def verify_signature(pem, msg, sig):
-        prepared_key = VerifyingKey.from_pem(pem)
-        prepared_msg = (urllib.parse.unquote(msg)).encode("utf-8")
+    def get_signature(self, request: HttpRequest) -> str:
+        encoded_signature = request.GET[self.SIGNATURE_PARAM_NAME]
 
         # Ensure that the signatures padding is always a multiple of 4. Note that
         # the decode function will ignore extraneous padding. Before the decode
         # method would occasionaly yield the following exception:
         # binascii.Error: Incorrect padding
-        prepared_sig = base64.urlsafe_b64decode(sig + "===")
+        return base64.urlsafe_b64decode(encoded_signature + "===")
+
+    def get_unverified_content(self, request: HttpRequest) -> str:
+        # According to the Admob SSV documentation, the last two query
+        # parameters of rewarded video SSV callbacks are always
+        # signature and key_id, in that order. The remaining query
+        # parameters specify the content to be verified.
+        query_string = request.META["QUERY_STRING"]
+        signature_start_index = query_string.index(f"&{self.SIGNATURE_PARAM_NAME}=")
+        encoded_content = query_string[:signature_start_index]
+        return urllib.parse.unquote(encoded_content)
+
+    def get_public_key(self, key_id: str) -> Optional[str]:
+        cached_public_keys = cache.get(settings.ADMOB_SSV_KEYS_CACHE_KEY, default={})
+        cached_public_key = cached_public_keys.get(key_id, None)
+
+        if cached_public_key is not None:
+            return cached_public_key
+
+        fetched_public_keys = self.fetch_public_keys()
+        cache.set(
+            settings.ADMOB_SSV_KEYS_CACHE_KEY,
+            fetched_public_keys,
+            math.floor(settings.ADMOB_SSV_KEYS_CACHE_TIMEOUT.total_seconds()),
+        )
+        return fetched_public_keys.get(key_id, None)
+
+    def fetch_public_keys(self) -> Dict[str, str]:
+        response = requests.get(settings.ADMOB_SSV_KEYS_SERVER_URL)
+        response.raise_for_status()
+        json_data = response.json()
+        return {str(key["keyId"]): key["pem"] for key in json_data["keys"]}
+
+    def verify_signature(self, public_key: str, signature: str, content: str) -> bool:
+        from ecdsa import VerifyingKey, BadSignatureError
+        from ecdsa.util import sigdecode_der
+
+        verifying_key = VerifyingKey.from_pem(public_key)
 
         try:
-            return prepared_key.verify(
-                prepared_sig,
-                prepared_msg,
+            return verifying_key.verify(
+                signature,
+                content,
                 hashfunc=hashlib.sha256,
                 sigdecode=sigdecode_der,
             )
