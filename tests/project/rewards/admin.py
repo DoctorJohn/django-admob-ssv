506ccc73d6179d0494564c9c6933dfcd55f47298
Jonathan Ehwald <github@ehwald.info>
2023-05-12 17:46:31 +0200

    Make admin give better overview

--- tests/project/rewards/admin.py
+++ tests/project/rewards/admin.py
@@ -2,4 +2,28 @@ from django.contrib import admin
 from .models import Reward
 
 
-admin.site.register(Reward)
+class RewardAdmin(admin.ModelAdmin):
+    list_display = (
+        "transaction_id",
+        "timestamp",
+        "ad_network",
+        "ad_unit",
+        "reward_item",
+        "reward_amount",
+        "user_id",
+    )
+    list_filter = (
+        "ad_network",
+        "ad_unit",
+        "reward_item",
+        "reward_amount",
+    )
+    search_fields = (
+        "transaction_id",
+        "user_id",
+        "custom_data",
+        "signature",
+    )
+
+
+admin.site.register(Reward, RewardAdmin)
